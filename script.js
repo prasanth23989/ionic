d2feb0b7189fc66213196770d46d14c96655987d
prasanth23989 <prasanth23989@gmail.com>
2023-02-04 17:40:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
-header {
-    background: #343E59 !important;
-}
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import HistoryIcon from '@mui/icons-material/History';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import { ListItemIcon } from '@mui/material';
 
-body {
-    background: linear-gradient(45deg,#3a3a60,#5f5f8e);
-    min-height: 100vh;
-  }
+export default function SideDrawerTabNav({setTabView}) {
+  const [state, setState] = React.useState({
+    left: false
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Divider />
+      <List>
+        {['', 'History', 'Current'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton onClick={() => {setTabView(text)}} >
+             <ListItemIcon>    
+              {text!='' && (text=='History' ? <HistoryIcon /> : <DashboardIcon />) }
+            </ListItemIcon>   
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+        <React.Fragment key={'left'}>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer('left', true)} >
+            <MenuIcon />
+          </IconButton> 
+          <Drawer anchor={'left'}  open={state['left']} onClose={toggleDrawer('left', false)}>
+            {list('left')}
+          </Drawer>
+        </React.Fragment>
+    </div>
+  );
+}

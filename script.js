0a2dda77f68164d39a1b9655b5c9fb86ce52e838
prasanth23989 <prasanth23989@gmail.com>
2023-02-06 11:06:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+<svg width="10" height="10" viewbox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="20" cy="20" fill="#FF0000" r="10" stroke="#FF0000" stroke-width="2">
+    <animate attributeName="r" from="8" to="20" dur="1.5s" begin="0s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" from="1" to="0" dur="1.5s" begin="0s" repeatCount="indefinite"/>
+  </circle>
+  <circle cx="20" cy="20" fill="#FF0000" r="10"/>
+</svg>
+
+
+html {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}

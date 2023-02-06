0c55d978d0d7c9b79a2436ed3c676ca6f8da4618
prasanth23989 <prasanth23989@gmail.com>
2023-02-06 12:01:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,79 @@
-<svg width="10" height="10" viewbox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="20" cy="20" fill="#FF0000" r="10" stroke="#FF0000" stroke-width="2">
-    <animate attributeName="r" from="8" to="20" dur="1.5s" begin="0s" repeatCount="indefinite"/>
-    <animate attributeName="opacity" from="1" to="0" dur="1.5s" begin="0s" repeatCount="indefinite"/>
-  </circle>
-  <circle cx="20" cy="20" fill="#FF0000" r="10"/>
-</svg>
-
-
-html {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+this.state = {
+          
+            series: [{
+              name: 'Marine Sprite',
+              data: [44, 55, 41, 37, 22, 43, 21]
+            }, {
+              name: 'Striking Calf',
+              data: [53, 32, 33, 52, 13, 43, 32]
+            }, {
+              name: 'Tank Picture',
+              data: [12, 17, 11, 9, 15, 11, 20]
+            }, {
+              name: 'Bucket Slope',
+              data: [9, 7, 5, 8, 6, 9, 4]
+            }, {
+              name: 'Reborn Kid',
+              data: [25, 12, 19, 32, 25, 24, 10]
+            }],
+            options: {
+              chart: {
+                type: 'bar',
+                height: 350,
+                stacked: true,
+              },
+              plotOptions: {
+                bar: {
+                  horizontal: true,
+                  dataLabels: {
+                    total: {
+                      enabled: true,
+                      offsetX: 0,
+                      style: {
+                        fontSize: '13px',
+                        fontWeight: 900
+                      }
+                    }
+                  }
+                },
+              },
+              stroke: {
+                width: 1,
+                colors: ['#fff']
+              },
+              title: {
+                text: 'Fiction Books Sales'
+              },
+              xaxis: {
+                categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+                labels: {
+                  formatter: function (val) {
+                    return val + "K"
+                  }
+                }
+              },
+              yaxis: {
+                title: {
+                  text: undefined
+                },
+              },
+              tooltip: {
+                y: {
+                  formatter: function (val) {
+                    return val + "K"
+                  }
+                }
+              },
+              fill: {
+                opacity: 1
+              },
+              legend: {
+                position: 'top',
+                horizontalAlign: 'left',
+                offsetX: 40
+              }
+            },
+          
+          
+          };
+        }
